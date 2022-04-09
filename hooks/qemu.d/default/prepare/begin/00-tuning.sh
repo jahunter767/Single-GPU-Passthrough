3819d5e05bc6d18242d3cd10a367b40cbb23d759
Jason Hunter <jahunter767@gmail.com>
2022-04-09 13:16:05 -0500

    automated thread pinning

--- hooks/qemu.d/default/prepare/begin/00-tuning.sh
+++ hooks/qemu.d/default/prepare/begin/00-tuning.sh
@@ -2,17 +2,48 @@
 
 # Isolate Cores
 function isolate_cores {
-    # @TODO: update this function to detect existing pinned threads, determine
-    #        if the threads in the list are already pinned, then pin the ones
-    #        that aren't pinned and save that list of newly pinned threads.
-    #        Save the details for thread pinning under the folder
-    #        ${TMP_CONFIG_PATH}/state/tuning
+    readarray -t all_threads <<< $(ls -dv /dev/cpu/*)
 
-    # @TODO: make the string comma separated
-    free_cores="${CPU_THREAD_LIST[*]}"
+    # Generates a string listing all the currently unpinned threads
+    function parse_free_threads {
+        local allowed_threads="${@}"
+        if [[ -n "${allowed_threads}" ]]; then
+            for c in ${allowed_threads}; do
+                if [[ "${c}" =~ ([0-9]+)-([0-9]+) ]]; then
+                    local start="${c%-*}"
+                    local end="${c#*-}"
+                    for (( i = ${start}; i <= ${end}; i++ )); do
+                        local free_threads="${free_threads} ${i} "
+                    done
+                elif [[ "${c}" =~ ([0-9]+) ]]; then
+                    local free_threads="${free_threads} ${c} "
+                fi
+            done
+        else
+            local free_threads=" ${all_threads[@]#/dev/cpu/} "
+        fi
 
-    # systemctl set-property --runtime -- user.slice AllowedCPUs=${free_cores}
-    # systemctl set-property --runtime -- system.slice AllowedCPUs=${free_cores}
-    # systemctl set-property --runtime -- init.scope AllowedCPUs=${free_cores}
-    echo ${free_cores}
+        echo "${free_threads}"
+    } # End-parse_free_threads
+
+    local allowed_user_threads="$(parse_free_threads \
+        "$(systemctl show -P AllowedCPUs user.slice)")"
+    local allowed_system_threads="$(parse_free_threads \
+        "$(systemctl show -P AllowedCPUs system.slice)")"
+    local allowed_init_threads="$(parse_free_threads \
+        "$(systemctl show -P AllowedCPUs init.slice)")"
+
+    # Removes the threads to be pinned from the list
+    for c in ${CPU_THREAD_LIST[@]}; do
+        local allowed_user_threads="${allowed_user_threads/ ${c} / }"
+        local allowed_system_threads="${allowed_system_threads/ ${c} / }"
+        local allowed_init_threads="${allowed_init_threads/ ${c} / }"
+    done
+
+    # systemctl set-property --runtime -- user.slice AllowedCPUs="${allowed_user_threads}"
+    # systemctl set-property --runtime -- system.slice AllowedCPUs="${allowed_system_threads}"
+    # systemctl set-property --runtime -- init.scope AllowedCPUs="${allowed_init_threads}"
+    echo "Free user.slice threads: ${allowed_user_threads}"
+    echo "Free system.slice threads: ${allowed_system_threads}"
+    echo "Free init.slice threads: ${allowed_init_threads}"
 } # End-isolate_cores
--- hooks/qemu.d/default/release/end/00-tuning.sh
+++ hooks/qemu.d/default/release/end/00-tuning.sh
@@ -2,18 +2,31 @@
 
 # Undoing core isolation
 function release_cores {
-    # @TODO: update this function to read system state before starting the VM
-    #        from ${TMP_CONFIG_PATH}/state/tuning/*.val then revert the
-    #        properties to what they were before.
-    #        It might be worth considering detecting the properties of other
-    #        VMs to determine if any currently running VMs that might have
-    #        been started after the current one required similar settings
+    local allowed_user_threads="${default_user_allowedCPUs}"
+    local allowed_system_threads="${default_system_allowedCPUs}"
+    local allowed_init_threads="${default_init_allowedCPUs}"
 
-    # @TODO: make the string comma separated
-    free_cores="${CPU_THREAD_LIST[*]}"
+    # Generated a list of threads pinned for other running VMs
+    local pinned_threads=""
+    for vm in $(ls -d1 ${TMP_CONFIG_PATH}/../*); do
+        if [[ "$(realpath ${vm})" != "${TMP_CONFIG_PATH}" ]]; then
+            local pinned_threads="${pinned_threads} \
+                $(get_hostdev_list "${vm}/domain/cputune/vcpupin" \
+                    "get_host_thread") "
+        fi
+    done
 
-    # systemctl set-property --runtime -- user.slice AllowedCPUs=""
-    # systemctl set-property --runtime -- system.slice AllowedCPUs=""
-    # systemctl set-property --runtime -- init.scope AllowedCPUs=""
-    echo "release cores ${free_cores}"
+    # Removes the pinned threads from the default list of unpinned threads
+    for c in ${pinned_threads}; do
+        local allowed_user_threads="${allowed_user_threads/ ${c} / }"
+        local allowed_system_threads="${allowed_system_threads/ ${c} / }"
+        local allowed_init_threads="${allowed_init_threads/ ${c} / }"
+    done
+
+    # systemctl set-property --runtime -- user.slice AllowedCPUs="${allowed_user_threads}"
+    # systemctl set-property --runtime -- system.slice AllowedCPUs="${allowed_system_threads}"
+    # systemctl set-property --runtime -- init.scope AllowedCPUs="${allowed_init_threads}"
+    echo "Free user.slice threads: ${allowed_user_threads}"
+    echo "Free system.slice threads: ${allowed_system_threads}"
+    echo "Free init.slice threads: ${allowed_init_threads}"
 } # End-release_cores
